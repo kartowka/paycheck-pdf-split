5c17660b21d948181c0bc01f8970d92ef1566942
kartowka <antonfley@gmail.com>
2023-06-30 09:18:34 +0300

    pdf splitter

---
+++ README.md
@@ -0,0 +1,41 @@
+# Gambrinus
+
+## Description
+
+IL Default Paycheck pdf splitter
+This app will split multi page pdf file into seperate top tree of files like this
+
+- Gambrinus
+  - employee name
+    - id_month_year.pdf
+
+## Table of Contents (Optional)
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Alternative](#alternative)
+
+## Prerequisites
+
+python3,python3-tk installed
+
+## Installation
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+
+```sh
+python3 file_open.py
+```
+
+## Alternative
+
+to make an executable app
+
+```sh
+pyinstaller --onefile file_open.py
+```
---
+++ file_open.py
@@ -0,0 +1,70 @@
+import sys
+import tkinter as tk
+from tkinter import Canvas, Label, Menu, PhotoImage, ttk
+from tkinter import filedialog
+from PIL import ImageTk, Image
+from tkinter.messagebox import showerror, showwarning, showinfo
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+
+def pdf_splitter(path):
+    pdf = PdfReader(path)
+    dir_name = 'Gambrinus'
+    if not os.path.exists(f'{os.getcwd()}/{dir_name}'):
+        os.mkdir(dir_name)
+    for page_number in range(len(pdf.pages)):
+        id = None
+        date = None
+        name = None
+        writer = PdfWriter()
+        writer.add_page(pdf.pages[page_number])
+        page = pdf.pages[page_number].extract_text()
+        lines = page.split('\n')
+        lines = iter(lines)
+        for line in lines:
+            if "לכבוד" in line:
+                name = next(lines)
+            if "תלוש שכר לחודש" in line:
+                date = (line.replace('/', '_')).split(" ")[0]
+            if "מספר זהות" in line:
+                id = line.split(" ")[0]
+        if date != None and id != None:
+            if not os.path.exists(f'{os.getcwd()}/{dir_name}/{name}'):
+                os.mkdir(f'{dir_name}/{name}')
+            filename = f'{dir_name}/{name}/{id}_{date}.pdf'
+            writer.encrypt(id)
+            with open(filename, 'wb') as output:
+                writer.write(output)
+                writer.close()
+        else:
+            showerror('Gambrinus', 'cannot create PDF files.')
+            sys.exit()
+
+
+def is_pdf():
+    global file_path
+    file_path = filedialog.askopenfilename()
+    file = file_path.lower().endswith(".pdf")
+    if file:
+        pdf_splitter(file_path)
+        showinfo('Gambrinus', 'PDF split complete.')
+        sys.exit()
+    else:
+        showerror("Gambrinus", "The selected file is not a PDF.")
+
+
+file_path = ''
+# create the root window
+root = tk.Tk()
+root.title('Gambrinus')
+root.geometry("400x300")
+root.resizable(False, False)
+options = {'fill': 'both', 'padx': 10, 'pady': 10, 'ipadx': 5}
+
+button = ttk.Button(root,
+                    text='Open file',
+                    command=is_pdf
+                    ).pack(**options)
+# run the app
+root.mainloop()
